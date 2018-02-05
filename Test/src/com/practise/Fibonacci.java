197f2abbebb7d56cbdd3aff8e55c093894fe141b
asif <syedasifuddin92@gmail.com>
2018-02-05 14:06:00 -0600

    First commit

---
+++ Test/src/com/practise/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.practise;
+
+public class Fibonacci {
+	public static void main(String[] args){
+		fibo(3);
+	}
+	
+	private static void fibo(int n){
+		int a= 0;
+		int b= 1;
+		
+		for(int i=0; i<n;i++){
+			int c = a + b;
+			a = b;
+			b = c;
+			System.out.println(a);
+		}
+	}
+}
